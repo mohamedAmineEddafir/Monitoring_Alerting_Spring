1b2f332f3aed1aabc4bca8d7bfb3d6b766e77d84
eddafir.mohamed.amine01 <eddafir.mohamed.amine01@gmail.com>
2025-01-28 16:16:08 +0100

    Add FactureRequestDTO for invoice request handling

--- src/main/java/com/banking/facturation/DTO/FactureRequestDTO.java
+++ src/main/java/com/banking/facturation/DTO/FactureRequestDTO.java
@@ -1,11 +1,8 @@
 package com.banking.facturation.DTO;
 
-import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
-@Data
 @AllArgsConstructor // Constructor with All parameter
 @NoArgsConstructor // Constructor without parameter
 public class FactureRequestDTO {

bd0577c36768a2862f0a317d64da6d8f181e054a
eddafir.mohamed.amine01 <eddafir.mohamed.amine01@gmail.com>
2025-01-28 16:13:22 +0100

    Compte Entity

--- src/main/java/com/banking/facturation/models/Compte.java
+++ src/main/java/com/banking/facturation/models/Compte.java
@@ -8,12 +8,12 @@ import jakarta.persistence.*;
 import java.math.BigDecimal;
 import java.io.Serializable;
 import java.util.List;
+import java.util.Objects;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.NoArgsConstructor;
 
 
-@Data
 @Entity
 @Table(name = "compte")
 @NoArgsConstructor
@@ -43,4 +43,70 @@ public class Compte implements Serializable {
     @OneToMany(mappedBy = "compte",cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Facture> factures;
 
+    // getter && setter
+
+    public int getIdCompte() {
+        return idCompte;
+    }
+
+    public void setIdCompte(int idCompte) {
+        this.idCompte = idCompte;
+    }
+
+    public BigDecimal getSolde() {
+        return solde;
+    }
+
+    public void setSolde(BigDecimal solde) {
+        this.solde = solde;
+    }
+
+    public LocalDate getDateCreation() {
+        return dateCreation;
+    }
+
+    public void setDateCreation(LocalDate dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public List<Facture> getFactures() {
+        return factures;
+    }
+
+    public void setFactures(List<Facture> factures) {
+        this.factures = factures;
+    }
+
+    // hashCode && equals
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Compte compte = (Compte) o;
+        return idCompte == compte.idCompte && Objects.equals(solde, compte.solde) && Objects.equals(dateCreation, compte.dateCreation) && Objects.equals(client, compte.client) && Objects.equals(factures, compte.factures);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idCompte, solde, dateCreation, client, factures);
+    }
+
+    @Override
+    public String toString() {
+        return "Compte{" +
+                "idCompte=" + idCompte +
+                ", solde=" + solde +
+                ", dateCreation=" + dateCreation +
+                ", client=" + client +
+                ", factures=" + factures +
+                '}';
+    }
 }

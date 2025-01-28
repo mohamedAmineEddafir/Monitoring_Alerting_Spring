150362ca52e919f1bf2e4ba6f6f189fd2286a7b2
eddafir.mohamed.amine01 <eddafir.mohamed.amine01@gmail.com>
2025-01-28 16:12:53 +0100

    Client Entity

--- src/main/java/com/banking/facturation/models/Client.java
+++ src/main/java/com/banking/facturation/models/Client.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import java.io.Serializable;
 import java.util.List;
+import java.util.Objects;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
@@ -12,7 +13,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 
-@Data
 @Entity
 @Table(name = "client")
 @NoArgsConstructor
@@ -41,20 +41,6 @@ public class Client implements Serializable {
     @Column(name = "telephone")
     private String telephone;
 
-    // Getter's
-    public String getNom(){
-        return nom;
-    }
-    public String getPrenom(){
-        return prenom;
-    }
-    public String getEmail(){
-        return email;
-    }
-    public String getTelephone(){
-        return telephone;
-    }
-
     /*
      * Relationships Between Entity's
      */
@@ -67,5 +53,88 @@ public class Client implements Serializable {
     @OneToMany(mappedBy = "client", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Facture> factures;
 
+    // getter && setters
 
+    public int getIdClient() {
+        return idClient;
+    }
+
+    public void setIdClient(int idClient) {
+        this.idClient = idClient;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public Compte getCompte() {
+        return compte;
+    }
+
+    public void setCompte(Compte compte) {
+        this.compte = compte;
+    }
+
+    public List<Facture> getFactures() {
+        return factures;
+    }
+
+    public void setFactures(List<Facture> factures) {
+        this.factures = factures;
+    }
+
+    // hashCode && equals
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return idClient == client.idClient && Objects.equals(nom, client.nom) && Objects.equals(prenom, client.prenom) && Objects.equals(email, client.email) && Objects.equals(telephone, client.telephone) && Objects.equals(compte, client.compte) && Objects.equals(factures, client.factures);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idClient, nom, prenom, email, telephone, compte, factures);
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "idClient=" + idClient +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", email='" + email + '\'' +
+                ", telephone='" + telephone + '\'' +
+                ", compte=" + compte +
+                ", factures=" + factures +
+                '}';
+    }
 }
